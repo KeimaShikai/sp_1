43dee3d89f9033534bb5657c7be3758306e37371
keimashikai <shikamaru740@gmail.com>
2019-03-21 12:23:23 +0700

    Add templates

--- Sources/hdr/container.h
+++ Sources/hdr/container.h
@@ -1,25 +1,93 @@
-#include <stack>
-#include <vector>
 #include <iostream>
 #include <fstream>
+
 
 #ifndef CONTAINER_H
 #define CONTAINER_H
 
+template <class T1>
 class container
 {
 private:
-    std::stack<double> sample;
+    T1 sample;
 
 public:
     container() {}
     ~container() {}
     void fill(const char *fName);
     void change(int n);
-    void change(int n, std::stack<double> in);
-    void show();
-    void show_iter();
-    std::stack<double> get_stack();
+    void change(int n, T1 in);
+    T1 get_data();
 };
+
+template <class T1>
+void container<T1>::fill(const char *fName)
+{
+    //fill container with the data from file
+    std::ifstream is(fName);
+    if (!is.is_open())
+    {
+        std::cout << "Can't open the '" << *fName << "' file!\n";
+        exit(0);
+    }
+    else
+    {
+        for(double d; is >> d;)
+            sample.emplace(d);
+    }
+    is.close();
+}
+
+template <class T1>
+void container<T1>::change(int n)
+{
+    //remove some elements
+    for (int i = 0; i < n; ++i)
+        sample.pop();
+
+    //change the others
+    std::cout << "Now change other " << sample.size() << " elements.\n";
+    T1 temp;
+    double elem = 0;
+    for (int i = 0; i < sample.size(); ++i)
+    {
+        std::cout << "New " << i + 1 << " element: ";
+        std::cin >> elem;
+        temp.emplace(elem);
+    }
+    std::swap(sample, temp);
+}
+
+template <class T1>
+void container<T1>::change(int n, T1 in)
+{
+    //delete n elements
+    int counter = 0;
+    if (n > sample.size())
+    {
+        std::cout << "The number is too big!\n"
+            << "The program wiil delete all items in the stack!\n";
+        counter = sample.size();
+    }
+    else
+    {
+        counter = n;
+    }
+    for (int i = 0; i < counter; ++i)
+        sample.pop();
+
+    //add all elements from second stack
+    while (!in.empty())
+    {
+        sample.emplace(in.top());
+        in.pop();
+    }
+}
+
+template <class T1>
+T1 container<T1>::get_data()
+{
+    return sample;
+}
 
 #endif
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,30 +1,84 @@
 #include "container.h"
+#include <stack>
+#include <queue>
+#include <vector>
+#include <iostream>
+
+
+template <class T1, class T2 = std::stack<double>>
+T2 converter(T1);
+
+void show(std::stack<double>);
+
+void show_iter(std::stack<double>);
+
 
 int main()
 {
     //1
-    container sample1;
+    container<std::stack<double>> sample1;
     sample1.fill("Files/for1");
     //2
     std::cout << "Container A: ";
-    sample1.show();
+    show(sample1.get_data());
     //3
+    std::cout << "Two items were delited from container A!\n";
     sample1.change(2);
     //4
     std::cout << "Container A*: ";
-    sample1.show_iter();
+    show_iter(sample1.get_data());
     //5
-    container sample2;
+    container<std::queue<double>> sample2;
     sample2.fill("Files/for2");
     //6
     int n = 0;
     std::cout << "Input the number of items you want to delete: ";
     std::cin >> n;
-    sample1.change(n, sample2.get_stack());
+    sample1.change(n, converter(sample2.get_data()));
     //7
     std::cout << "Container A: ";
-    sample1.show();
+    show(sample1.get_data());
     std::cout << "Container B: ";
-    sample2.show();
+    show(converter(sample2.get_data()));
     return 0;
+}
+
+
+template <class T1, class T2>
+T2 converter(T1 temp1)
+{
+    T2 temp2;
+    while (!temp1.empty())
+    {
+        temp2.emplace(temp1.front());
+        temp1.pop();
+    }
+    return temp2;
+}
+
+void show(std::stack<double> sample)
+{
+    //output the stack
+    while (!sample.empty())
+    {
+        std::cout << sample.top() << " || ";
+        sample.pop();
+    }
+    std::cout << std::endl;
+}
+
+void show_iter(std::stack<double> sample)
+{
+    //output the stack via iterators
+    //codemonkey was here//
+    std::vector<double> vTemp;
+    while (!sample.empty())
+    {
+        vTemp.emplace_back(sample.top());
+        sample.pop();
+    }
+    for (std::vector<double>::iterator it = vTemp.begin();
+            it != vTemp.end(); ++it)
+        std::cout << *it << " || ";
+    std::cout << std::endl;
 }
